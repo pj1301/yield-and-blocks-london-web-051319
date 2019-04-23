5821b8044368c6f7c5f33b10bb1aa71bbb72dd9d
James Hayward <james.hayward6@icloud.com>
2019-04-23 19:00:02 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,17 @@
 def hello_t(array, name)
-  if array.length = 0
-    puts "Hey! No block was given!\n"
-  elsif array.length > 0
-   array.each {|name| puts name}
-  # array.cycle(1) {|x| puts x}
+  # if array.length = 0
+  #   puts "Hey! No block was given!\n"
+  # elsif array.length > 0
+  #  array.each {|name| puts name}
+  # # array.cycle(1) {|x| puts x}
 
+  i = o
+  while i < array.length
+    yield array[i]
+    i += 1
   end
+
+
 end
 
 # call your method here!
