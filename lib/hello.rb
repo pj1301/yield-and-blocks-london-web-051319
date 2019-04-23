47100a95798aa687fd1ce2806827b3a7ebd5578a
James Hayward <james.hayward6@icloud.com>
2019-04-23 18:54:06 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,7 +2,7 @@ def hello_t(array, name)
   if array.size = 0
     puts "Hey! No block was given!\n"
   elsif array.size > 0
-   puts array.each {|name| puts name}
+   array.each {|name| puts name}
   # array.cycle(1) {|x| puts x}
 
   end
